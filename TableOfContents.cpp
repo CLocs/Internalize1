a53dfffe9a1cc7d038b93a5e49a482b3da37ebe0
clocs <dascolin@gmail.com>
2017-08-25 23:43:16 -0400

    Adding more sources. But returns are coming up as weird characters. And 18 Mistakes content is not showing up.

--- TableOfContents.cpp
+++ TableOfContents.cpp
@@ -25,6 +25,7 @@ namespace {
 TableOfContents::TableOfContents(QObject *parent)
     : QStandardItemModel(parent)
 {
+    // Sections
     auto miscSection = new QStandardItem("Misc");
     miscSection->setData(randomColor(1),SectionColor);
     auto mindMachinery = new QStandardItem("Mind Machinery");
@@ -38,6 +39,9 @@ TableOfContents::TableOfContents(QObject *parent)
         appendRow(more);
     }
 
+    // Pages
+
+    // Section: Misc
     auto slateStarCodex = new QStandardItem();
     slateStarCodex->setData("Slate Star Codex",TableOfContents::Roles::Title);
     slateStarCodex->setData(ObjectType::WebPage,TableOfContents::Roles::ObjectType);
@@ -49,7 +53,6 @@ TableOfContents::TableOfContents(QObject *parent)
     internalizeOrgChart->setData(ObjectType::Image,TableOfContents::Roles::ObjectType);
     internalizeOrgChart->setData("Internalize-Demo1.svg",Image::Roles::URL);
     miscSection->appendRow(internalizeOrgChart);
-
 
     auto poem = new QStandardItem();
     poem->setData("Death of a Hired Man",TableOfContents::Roles::Title);
@@ -63,6 +66,7 @@ TableOfContents::TableOfContents(QObject *parent)
     untitled1->setData("",Note::Roles::Content);
     miscSection->appendRow(untitled1);
 
+    // Section: Mind Machinery
     auto thinkingFastAndSlow = new QStandardItem();
     thinkingFastAndSlow->setData("Thinking Fast and Slow",TableOfContents::Roles::Title);
     thinkingFastAndSlow->setData(ObjectType::Note,TableOfContents::Roles::ObjectType);
@@ -80,6 +84,48 @@ TableOfContents::TableOfContents(QObject *parent)
     onIntelligence->setData(ObjectType::Note,TableOfContents::Roles::ObjectType);
     onIntelligence->setData(readSourceFile(":/repository/on-intelligence.txt"),Note::Roles::Content);
     mindMachinery->appendRow(onIntelligence);
+
+    auto drive = new QStandardItem();
+    drive->setData("Drive",TableOfContents::Roles::Title);
+    drive->setData(ObjectType::Note,TableOfContents::Roles::ObjectType);
+    drive->setData(readSourceFile(":/repository/drive.txt"),Note::Roles::Content);
+    mindMachinery->appendRow(drive);
+
+    auto originals = new QStandardItem();
+    originals->setData("Originals",TableOfContents::Roles::Title);
+    originals->setData(ObjectType::Note,TableOfContents::Roles::ObjectType);
+    originals->setData(readSourceFile(":/repository/originals.txt"),Note::Roles::Content);
+    mindMachinery->appendRow(originals);
+
+    auto personality_myth = new QStandardItem();
+    personality_myth->setData("Personality Myth",TableOfContents::Roles::Title);
+    personality_myth->setData(ObjectType::Note,TableOfContents::Roles::ObjectType);
+    personality_myth->setData(readSourceFile(":/repository/personality_myth.txt"),Note::Roles::Content);
+    mindMachinery->appendRow(personality_myth);
+
+    auto skills_vs_talents = new QStandardItem();
+    skills_vs_talents->setData("Skills vs Talents",TableOfContents::Roles::Title);
+    skills_vs_talents->setData(ObjectType::Note,TableOfContents::Roles::ObjectType);
+    skills_vs_talents->setData(readSourceFile(":/repository/personality_myth.txt"),Note::Roles::Content);
+    mindMachinery->appendRow(skills_vs_talents);
+
+    auto slowing_down = new QStandardItem();
+    slowing_down->setData("Slowing Down",TableOfContents::Roles::Title);
+    slowing_down->setData(ObjectType::Note,TableOfContents::Roles::ObjectType);
+    slowing_down->setData(readSourceFile(":/repository/slowing_down.txt"),Note::Roles::Content);
+    mindMachinery->appendRow(slowing_down);
+
+    auto the_18_mistakes_that_kill_startups = new QStandardItem();
+    the_18_mistakes_that_kill_startups->setData("The 18 Mistakes That Kill Startups",TableOfContents::Roles::Title);
+    the_18_mistakes_that_kill_startups->setData(ObjectType::Note,TableOfContents::Roles::ObjectType);
+    the_18_mistakes_that_kill_startups->setData(readSourceFile(":/repository/the_18_mistakes_that_kill_startups.txt"),Note::Roles::Content);
+    mindMachinery->appendRow(the_18_mistakes_that_kill_startups);
+
+    auto tim_ferriss_187_jocko = new QStandardItem();
+    tim_ferriss_187_jocko->setData("Tim Ferriss 187 - Jocko Willink",TableOfContents::Roles::Title);
+    tim_ferriss_187_jocko->setData(ObjectType::Note,TableOfContents::Roles::ObjectType);
+    tim_ferriss_187_jocko->setData(readSourceFile(":/repository/tim_ferriss_187_jocko.txt"),Note::Roles::Content);
+    mindMachinery->appendRow(tim_ferriss_187_jocko);
 }
 
 // Second, define the singleton type provider function (callback).
